a613efa8684fe1d33004eb4b6ce800ba15975a2c
Chester Heng <chester.heng@gmail.com>
2019-11-13 06:05:51 +0800

    Other variable types

--- hello.py
+++ hello.py
@@ -1,11 +1,5 @@
-# Create a variable savings
-savings = 100
+# Create a variable desc
+desc = "compound interest"
 
-# Create a variable growth_multiplier
-growth_multiplier = 1.1
-
-# Calculate result
-result = savings * growth_multiplier ** 7
-
-# Print out result
-print(result)
+# Create a variable profitable
+profitable = True
