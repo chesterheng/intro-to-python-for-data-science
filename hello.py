905c7fb733434fba2812a42cdf2af0803f85165f
Chester Heng <chester.heng@gmail.com>
2019-11-13 07:07:01 +0800

    Selective import

--- hello.py
+++ hello.py
@@ -1,15 +1,11 @@
 # Definition of radius
-r = 0.43
+r = 192500
 
-# Import the math package
-import math
+# Import radians function of math package
+from math import radians
 
-# Calculate C
-C = 2 * math.pi * r
+# Travel distance of Moon over 12 degrees. Store in dist.
+dist = r * radians(12)
 
-# Calculate A
-A = math.pi * r ** 2
-
-# Build printout
-print("Circumference: " + str(C))
-print("Area: " + str(A))
+# Print out dist
+print(dist)
