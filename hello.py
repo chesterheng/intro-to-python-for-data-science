9843284fe7b94e2859b6d276f8fd1e3d7d92f69d
Chester Heng <chester.heng@gmail.com>
2019-11-13 06:14:59 +0800

    Type conversion

--- hello.py
+++ hello.py
@@ -1,15 +1,12 @@
+# Definition of savings and result
 savings = 100
-growth_multiplier = 1.1
-desc = "compound interest"
+result = 100 * 1.10 ** 7
 
-# Assign product of growth_multiplier and savings to year1
-year1 = savings * growth_multiplier
+# Fix the printout
+print("I started with $" + str(savings) + " and now have $" + str(result) + ". Awesome!")
 
-# Print the type of year1
-print(type(year1))
+# Definition of pi_string
+pi_string = "3.1415926"
 
-# Assign sum of desc and desc to doubledesc
-doubledesc = desc + desc
-
-# Print out doubledesc
-print(doubledesc)
+# Convert pi_string into float: pi_float
+pi_float = float(pi_string)
