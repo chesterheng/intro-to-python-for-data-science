3faaa4b402d5263287caaf665f4f7d5a74779999
Chester Heng <chester.heng@gmail.com>
2019-11-13 06:02:16 +0800

    Variable Assignment

--- hello.py
+++ hello.py
@@ -1,12 +1,5 @@
-# Addition, subtraction
-print(5 + 5)
-print(5 - 5)
+# Create a variable savings
+savings = 100
 
-# Multiplication, division, modulo, and exponentiation
-print(3 * 5)
-print(10 / 2)
-print(18 % 7)
-print(4 ** 2)
-
-# How much is your $100 worth after 7 years?
-print (1.1 ** 7 * 100)
+# Print out savings
+print(savings)
